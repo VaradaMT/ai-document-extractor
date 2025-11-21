efe9d3e0f0990b3ffcebacb3572ced719af37746
VaradaMT <varadamt.ds24@duk.ac.in>
2025-11-21 11:50:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from extractor import extract_text_from_pdf, extract_key_value_pairs, save_to_excel
 
-
 st.title("AI Document Structuring Tool")
 
 uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
@@ -11,9 +10,12 @@ if uploaded_file:
         f.write(uploaded_file.getvalue())
 
     text = extract_text_from_pdf("temp.pdf")
-    pairs = detect_key_value_pairs(text)
+    pairs = extract_key_value_pairs(text)
+
     output_file = save_to_excel(pairs)
 
-    st.success("Extraction completed!")
-    st.download_button("Download Excel", open(output_file, "rb"), file_name="Output.xlsx")
+    st.success("Extraction completed successfully!")
+    st.download_button("Download Excel", open(output_file, "rb").read(), file_name="Output.xlsx")
+
+
 
